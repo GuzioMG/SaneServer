821897295b477b363a4a3457de27fa44a7b2d2da
Guzio <guzekk@protonmail.com>
2026-03-04 21:51:02 +0100

    added IntelliJ

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "hub.guzio.TrixnityTest"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("de.connect2x.trixnity:trixnity-bom:5.1.2"))
+
+    implementation("de.connect2x.trixnity:trixnity-client")
+
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
